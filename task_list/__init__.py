8174ca428272e12e70cf81b20c7e390f4a3889f2
Sascha <sascha.trifunovic@gmail.com>
2018-08-09 14:26:51 +0200

    Database setup

--- task_list/__init__.py
+++ task_list/__init__.py
@@ -1,10 +1,23 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
-        SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key'
+        SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(app.instance_path, 'task-list.sqlite'),
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
     )
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from . import models
 
     return app
