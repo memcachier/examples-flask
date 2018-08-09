1cf57f455bd6dc4b60d2279671409ede6b94de14
Sascha <sascha.trifunovic@gmail.com>
2018-08-09 16:03:59 +0200

    Add caching with MemCachier

--- task_list/__init__.py
+++ task_list/__init__.py
@@ -2,7 +2,9 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_caching import Cache
 
+cache = Cache()
 db = SQLAlchemy()
 migrate = Migrate()
 
@@ -17,6 +19,33 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    cache_servers = os.environ.get('MEMCACHIER_SERVERS')
+    if cache_servers == None:
+        cache.init_app(app, config={'CACHE_TYPE': 'simple'})
+    else:
+        cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
+        cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
+        cache.init_app(app,
+            config={'CACHE_TYPE': 'saslmemcached',
+                    'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
+                    'CACHE_MEMCACHED_USERNAME': cache_user,
+                    'CACHE_MEMCACHED_PASSWORD': cache_pass,
+                    'CACHE_OPTIONS': { 'behaviors': {
+                        # Faster IO
+                        'tcp_nodelay': True,
+                        # Keep connection alive
+                        'tcp_keepalive': True,
+                        # Timeout for set/get requests
+                        'connect_timeout': 2000, # ms
+                        'send_timeout': 750 * 1000, # us
+                        'receive_timeout': 750 * 1000, # us
+                        '_poll_timeout': 2000, # ms
+                        # Better failover
+                        'ketama': True,
+                        'remove_failed': 1,
+                        'retry_timeout': 2,
+                        'dead_timeout': 30}}})
 
     from . import models
 
