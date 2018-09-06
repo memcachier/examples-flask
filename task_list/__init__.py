c087cd70dc64ed90fd01e5905e847f3f759ad40d
Lindsey Jacks <linzjax@gmail.com>
2018-09-06 10:43:06 -0400

    Database setup

--- task_list/__init__.py
+++ task_list/__init__.py
@@ -1,10 +1,34 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
+    if 'RDS_HOSTNAME' in os.environ:
+        DATABASE = {
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+
+    database_url = 'postgresql://%(USER)s:%(PASSWORD)s@%(HOST)s:%(PORT)s/%(NAME)s' % DATABASE
+
     app = Flask(__name__)
     app.config.from_mapping(
-        SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key'
+        SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
+        SQLALCHEMY_DATABASE_URI = database_url or \
+            'sqlite:///' + os.path.join(app.instance_path, 'task_list.sqlite'),
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
     )
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from . import models
 
     return app
